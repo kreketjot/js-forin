16f78c205315a72e86ffb03e5db5096469242b99
Andrey Vinokurov <kreketjot@gmail.com>
2021-09-07 22:21:26 +0300

    added output to page index.html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import orderByProps from './orderByProps';
 
-const obj = {
+const character = {
   name: 'мечник',
   health: 10,
   level: 2,
   attack: 80,
   defence: 40,
 };
-const result = orderByProps(obj, ['name', 'level']);
-console.log(result);
+const result = orderByProps(character, ['name', 'level']);
+result.forEach((obj) => {
+  const p = document.createElement('p');
+  p.innerText = `key: ${obj.key}, value: ${obj.value}`;
+  document.body.append(p);
+});
